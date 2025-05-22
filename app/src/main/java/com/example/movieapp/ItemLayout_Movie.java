739a837a6fb669d54b5ff0c3a3cee5b70e3690d1
HoangThanhTrang04 <tranght1811@gmail.com>
2025-05-22 22:01:49 +0700

    detail movie và booking

--- app/src/main/java/com/example/movieapp/ItemLayout_Movie.java
+++ app/src/main/java/com/example/movieapp/ItemLayout_Movie.java
@@ -1,6 +1,9 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +22,19 @@ public class ItemLayout_Movie extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        ImageButton btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(v -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Đóng Activity hiện tại
+        });
+
+        // 2. Nút đặt vé -> Sang BookingActivity
+        Button btnBookTicket = findViewById(R.id.btn_book_ticket);
+        btnBookTicket.setOnClickListener(v -> {
+            Intent intent = new Intent(this, BookingActivity.class);
+            startActivity(intent);
         });
     }
 }
